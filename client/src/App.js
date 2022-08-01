1018195254ebf40794c675062d315150108e4765
aaliosman <aliosman027@gmail.com>
2022-08-01 22:44:54 +0200

    edit app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-
 import React from 'react';
 import About from './Pages/about/About';
+import {Home} from './Pages/Home/Home';
 const App = (params) => {
     return(
         <div className="App" >
              
-             <About />
+             {/* <About /> */}
+             <Home />
 
         </div>
     )

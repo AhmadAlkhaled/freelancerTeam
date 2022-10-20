3f50b09f08be8a5f478a2f782d0b8d747a0274ea
Mustapha Ibrahim <Stexe@msn.com>
2022-10-20 16:50:56 -0400

    general problem edeting'S

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -1,73 +1,70 @@
-import Logo from '../../imgs/logo.png'
-import './Nav.scss'
-import React,  { useState, useEffect } from 'react'
-import {motion} from 'framer-motion' 
-import { NavLink } from 'react-router-dom';
+import Logo from "../../imgs/logo.png";
+import "./Nav.scss";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
 
-const Nav = (props) => { 
-    
-    const [hamburger, setHamburger] = useState (true);
-    const [mobileMode, setMobileMode] = useState (true);
-    
+const Nav = (props) => {
+  const [hamburger, setHamburger] = useState(true);
+  const [mobileMode, setMobileMode] = useState(true);
 
-    const windowSize = ()=>{
-        if(window.innerWidth < 700){
-            setMobileMode(false)
-        }
-        else{
-            setMobileMode(true)
-        }
+  const windowSize = () => {
+    if (window.innerWidth < 700) {
+      setMobileMode(false);
+    } else {
+      setMobileMode(true);
     }
+  };
 
-    window.addEventListener('resize', () => {
-        windowSize();
-    })
+  window.addEventListener("resize", () => {
+    windowSize();
+  });
 
-    useEffect (() => {
-        windowSize();
-        
-    },[])
+  useEffect(() => {
+    windowSize();
+  }, []);
 
-    return (
-        <div className="nav"
-        onClick={() =>{
-            props.setHamburgerAnimation(false)
-        }}
+  return (
+    <div
+      className="nav"
+      onClick={() => {
+        props.setHamburgerAnimation(false);
+      }}
+    >
+      <div className="logo" id="logo">
+        <NavLink to="/">
+          <img src={Logo} alt="Logo" />
+        </NavLink>
+      </div>
+
+      {hamburger ? (
+        <motion.div
+          className={mobileMode ? "navList" : "nav_mobile"}
+          initial={{ height: "0px" }}
+          animate={{ height: props.hamburgerAnimation ? "100vh" : "0px" }}
+          transition={{ duration: 0.6 }}
+          onClick={(e) => {
+            props.setHamburgerAnimation(false);
+            e.stopPropagation();
+          }}
         >
-            <div className="logo" id="logo">
-                <NavLink to='/'><img src={Logo} alt="Logo"/></NavLink>
-            </div>
+          <a href="/">Home</a>
+          <NavLink to="/About">About</NavLink>
+          <a href="/Contact">Contact</a>
+        </motion.div>
+      ) : null}
 
-            {
-                hamburger ?
-                <motion.div className={mobileMode ? 'navList' : 'nav_mobile' }
-                initial={{height:'0px'}}
-                animate={{height: props.hamburgerAnimation ? '500px' : '0px'}}
-                transition={{duration:0.6}}
-                onClick={(e) =>{
-                    props.setHamburgerAnimation(false)
-                    e.stopPropagation();
-                }}
-                >
-                <a href='/'>Home</a>
-                <NavLink to='/About'>About</NavLink>
-                <a href='/Contact'>Contact</a>
+      <div
+        className="hamburgerNav"
+        onClick={(e) => {
+          props.setHamburgerAnimation(!props.hamburgerAnimation);
+          e.stopPropagation();
+        }}
+      >
+        <i class="fas fa-bars"></i>
+      </div>
+    </div>
+  );
+};
 
-                </motion.div>
-                :
-                null
-            }
-            
-            <div className="hamburgerNav"
-                onClick={(e)=>{
-                    props.setHamburgerAnimation( !props.hamburgerAnimation );
-                    e.stopPropagation();
-                }}
-
-            ><i class="fas fa-bars"></i></div>
-            
-        </div>
-    )
- }
-
- export { Nav }
+export { Nav };
--- client/src/Pages/contact/Contact.js
+++ client/src/Pages/contact/Contact.js
@@ -1,11 +1,10 @@
-import  { useState } from 'react';
-import './Contact.scss';
-import Start from '../../Components/start/Start';
-import axios from 'axios' ;
+import { useState } from "react";
+import "./Contact.scss";
+import Start from "../../Components/start/Start";
+import axios from "axios";
 import { motion } from "framer-motion";
 
 const Contact = () => {
-
   const [submit, setsubmit] = useState(false);
   const [name, setName] = useState();
   const [email, setEmail] = useState();
@@ -16,122 +15,137 @@ const Contact = () => {
     name: name,
     email: email,
     subject: subject,
-    message: message
-  }
+    message: message,
+  };
 
   const SendMessage = () => {
-    if(name && email && subject && message) {
-      axios.post('/contact', MessageData)
-      .then((res) => {
+    if (name && email && subject && message) {
+      axios.post("/contact", MessageData).then((res) => {
         console.log(res);
-      })
+      });
     }
-  }
+  };
 
   return (
-    <div className='contact'>
-        <div className="contact-section-1">
-          
-          <div className="opacity">
-            <motion.div  className="contact-text"
-                initial={{ x: '-30%', opacity:0 }}
-                animate={{ x: '0' , opacity:1  }} 
-                transition={{ duration: 0.5 }}
-            >
-                  <h1>Contact Us</h1>
-                  <h3>Contact for Premium Business Services</h3>
-                  <p>We are happy to answer any questions you have about our services. <br /> To get in touch with us, please fill out the form below and we will get back to you within 24 hours.</p>
-              </motion.div >
-          </div>
-            
+    <div className="contact">
+      <div className="contact-section-1">
+        <div className="opacity">
+          <motion.div
+            className="contact-text"
+            initial={{ x: "-30%", opacity: 0 }}
+            animate={{ x: "0", opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <h1>Contact Us</h1>
+            <h4>Contact for Premium Business Services</h4>
+            <p>
+              We are happy to answer any questions you have about our services.{" "}
+              <br /> To get in touch with us, please fill out the form below and
+              we will get back to you within 24 hours.
+            </p>
+          </motion.div>
         </div>
+      </div>
 
-        <div className="form-section">
-            <div className="form-main">
-              <div className="left-box">
+      <div className="form-section">
+        <div className="form-main">
+          <div className="left-box">
+            {submit ? (
+              <p
+                style={{
+                  width: "60%",
+                  padding: " 0 20px",
+                  textAlign: "center",
+                  margin: " auto 0 ",
+                  textAlign: "center",
+                  fontSize: "20px",
+                  fontFamily: "Orienta,sans-serif",
+                }}
+              >
+                Thanks for contacting Us ! We will be in touch with you shortly.
+              </p>
+            ) : (
+              <form
+                action="/Contact"
+                method="post"
+                onSubmit={() => {
+                  setsubmit(true);
+                }}
+              >
+                <h3>Send Us A Message</h3>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <br />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  name="subject"
+                  required
+                  onChange={(e) => setSubject(e.target.value)}
+                />
+                <br />
+                <textarea
+                  id=""
+                  cols="30"
+                  rows="10"
+                  placeholder="Massage"
+                  name="massage"
+                  required
+                  onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+                <br />
+                <button onClick={SendMessage}>Send Message</button>
+              </form>
+            )}
 
-
-                { 
-                  (submit)?
-                    <p style={{ width: '60%' , padding : ' 0 20px'  , textAlign: 'center', margin: ' auto 0 '  , textAlign: 'center' 
-                     ,fontSize: '20px' , fontFamily:'Orienta,sans-serif' }} >Thanks for contacting Us ! We will be in touch with you shortly.</p>
-                  :
-                    <form action="/Contact" method="post" onSubmit={()=>{setsubmit(true)}} >
-                      <h3>Send Us A Message</h3>
-                      <input 
-                        type="text"    
-                        placeholder="Name" 
-                        name="name" 
-                        required 
-                        onChange={(e) => setName(e.target.value)}
-                        />
-                      <br />
-                      <input 
-                        type="text" 
-                        placeholder='Email' 
-                        name="email" 
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                        />
-                      <br />
-                      <input 
-                        type="text" 
-                        placeholder='Subject' 
-                        name="subject" 
-                        required
-                        onChange={(e) => setSubject(e.target.value)}
-                        />
-                      <br />
-                      <textarea 
-                        id="" 
-                        cols="30" 
-                        rows="10" 
-                        placeholder="Massage" 
-                        name="massage" 
-                        required
-                        onChange={(e) => setMessage(e.target.value)}
-                        ></textarea>
-                      <br />
-                      <button onClick={SendMessage} >Send Message</button>
-                    </form>
-                }
-                
-                 <div className="form-text">
-                    <h3>Get In Touch</h3>
-                    <h5>Address</h5>
-                    <p>Berlin – Germany</p>
-                    <h5>Email</h5>
-                    <p>info@example.com</p>
-                    <h5>Phone</h5>
-                    <p>+490000000000</p>
-                    <h5>Follow Us</h5>
-                    <div className="icons">
-                    <i className="fab fa-facebook"></i>
-                        <i className="fab fa-twitter"></i>
-                        <i className="fab fa-google"></i>
-                        <i className="fab fa-instagram"></i>
-                        <i className="fab fa-linkedin"></i>
-                    </div>
-                 
-                 </div>
-              </div>
-              <div className="right-box">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d155421.59372221289!2d13.284650302208142!3d52.506970379262846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2sBerlin!5e0!3m2!1sen!2sde!4v1659621024007!5m2!1sen!2sde"
-                width="100%"
-                height="100%"
-                frameBorder="0"
-                // style="border:0;"
-                allowFullScreen=""
-                aria-hidden="false"
-                tabIndex="0"
-            />
+            <div className="form-text">
+              <h3>Get In Touch</h3>
+              <h5>Address</h5>
+              <p>Berlin – Germany</p>
+              <h5>Email</h5>
+              <p>info@example.com</p>
+              <h5>Phone</h5>
+              <p>+490000000000</p>
+              <h5>Follow Us</h5>
+              <div className="icons">
+                <i className="fab fa-facebook"></i>
+                <i className="fab fa-twitter"></i>
+                <i className="fab fa-google"></i>
+                <i className="fab fa-instagram"></i>
+                <i className="fab fa-linkedin"></i>
               </div>
             </div>
+          </div>
+          <div className="right-box">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d155421.59372221289!2d13.284650302208142!3d52.506970379262846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2sBerlin!5e0!3m2!1sen!2sde!4v1659621024007!5m2!1sen!2sde"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              // style="border:0;"
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            />
+          </div>
         </div>
-        <Start />
+      </div>
+      <Start />
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
